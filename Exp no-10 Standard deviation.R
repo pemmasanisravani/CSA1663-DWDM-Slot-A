ef97726a59d29c66a876b5878a9a5df49b55c8fb
pemmasanisravani <124758436+pemmasanisravani@users.noreply.github.com>
2024-07-23 10:58:20 +0530

    Add files via upload

---
+++ Exp no-10 Standard deviation.R
@@ -0,0 +1,5 @@
+speeds <- c(78.3, 81.8, 82, 74.2, 83.4, 84.5, 82.9, 77.5, 80.9, 70.6)
+iqr <- IQR(speeds)
+std_dev <- sd(speeds)
+cat("Interquartile Range (IQR):", iqr, "\n")
+cat("Standard Deviation:", std_dev, "\n")
